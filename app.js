64b817bbfa6d63256f3e282c8c62bd7fba1f946e
MohammedOsamma <mo856915i@gmail.com>
2025-05-05 23:12:30 +0200

    Using Computed Properties with Classes & Making Dynamically Classes with Array Syntex

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const app = Vue.createApp({
       boxCSelected: false,
     };
   },
+  computed: {
+    BoxASelected() {
+      return { active: this.boxASelected };
+    },
+  },
   methods: {
     boxSelected(box) {
       if (box === "A") {
